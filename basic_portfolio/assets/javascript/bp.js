819d455be67088560e86a50e7ac383eec39fabe3
KingJeremy <kingjeremy2211@hotmail.com>
2018-09-08 22:50:34 -0500

    added animations

---
+++ basic_portfolio/assets/javascript/bp.js
@@ -0,0 +1,15 @@
+$( document ).ready(function(){
+
+    $('li').addClass('animated rollIn delay-3s'); 
+    $('.logo').addClass('animated tada');
+    $('p').addClass('animated bounceInUp');
+    $('.h1.page-title').addClass('animated fadeInUpBig delay-2s');
+    $('img.headshot').addClass('animated fadeIn delay-1s');
+    $('.social').addClass('animated rollIn');
+    $('.nav').addClass('animated zoomInDown');
+    $('.portfolio-item').addClass('animated zoomInUp');
+    $('main.p').addClass('animated pulse');
+
+});
+
+ 
