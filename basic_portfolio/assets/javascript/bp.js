3f97dc4b2f1f3e34c86a98a767bf8e2093d1be8e
KingJeremy <kingjeremy2211@hotmail.com>
2018-09-10 22:01:17 -0500

    final push

--- basic_portfolio/assets/javascript/bp.js
+++ basic_portfolio/assets/javascript/bp.js
@@ -1,14 +1,13 @@
 $( document ).ready(function(){
 
-    $('li').addClass('animated rollIn delay-2s'); 
+    $('li').addClass('animated rollIn delay-3s'); 
     $('.logo').addClass('animated tada');
     $('p').addClass('animated bounceInUp');
-    $('.h1.page-title').addClass('animated fadeInUpBig delay-1.5s');
+    $('.h1.page-title').addClass('animated fadeInUpBig delay-2s');
     $('img.headshot').addClass('animated fadeIn delay-1s');
     $('.social').addClass('animated rollIn');
     $('.nav').addClass('animated zoomInDown');
     $('.portfolio-item').addClass('animated zoomInUp');
-    $('main.p').addClass('animated pulse');
 
 });
 
